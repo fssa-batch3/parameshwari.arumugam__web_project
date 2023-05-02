57f78f50e8ff7d6c5f7b24994f449d06b659feab
Parameshwari2004 <parameshwariarumugam8@gmail.com>
2023-05-02 13:40:12 +0530

    fix: fix bugs in payment page

--- asset/js/orderhistory.js
+++ asset/js/orderhistory.js
@@ -5,7 +5,7 @@ let h4;
 let image_flower;
 
 let order_list = JSON.parse(localStorage.getItem("order"));
-let activeuser=JSON.parse(localStorage.getItem("active_user"));
+let activeuser = JSON.parse(localStorage.getItem("active_user"));
 
 // let order_sample=[
 // {
@@ -18,46 +18,42 @@ let activeuser=JSON.parse(localStorage.getItem("active_user"));
 // }
 // ]
 
+order_list.find((el) => {
+  if (activeuser.emailid === el.emailid) {
+    div_content = document.createElement("div");
+    div_content.setAttribute("class", "content");
+    // console.log(div_content);
+    // console.log(h2);
 
+    h2 = document.createElement("h2");
+    // document.querySelector("div.content").append(h2);
+    h2.setAttribute("id", "orderid");
+    h2.innerText = el["order_id"];
 
-for(i=0; i<order_list.length; i++){
+    div_content.append(h2);
+    console.log(h2);
 
+    h3 = document.createElement("h3");
+    h3.setAttribute("id", "order_date");
+    h3.setAttribute("class", "orderdate");
+    h3.innerText = el["order_date"];
+    div_content.append(h3);
 
-div_content = document.createElement("div");
-div_content.setAttribute("class", "content");
-// console.log(div_content);
-// console.log(h2);
+    h4 = document.createElement("h3");
+    h4.setAttribute("id", "rupees");
+    h4.setAttribute("class", "rupees");
+    h4.innerText = "₹" + el["price"];
+    div_content.append(h4);
 
-h2 = document.createElement("h2");
-// document.querySelector("div.content").append(h2);
-h2.setAttribute("id", "orderid");
-h2.innerText = order_list[i]["order_id"];
+    image_flower = document.createElement("img");
+    image_flower.setAttribute("id", "flower");
+    image_flower.setAttribute("src", el["img"]);
+    image_flower.setAttribute("alt", el["alt"]);
+    div_content.append(image_flower);
 
-div_content.append(h2);
-console.log(h2);
-
-h3 = document.createElement("h3");
-h3.setAttribute("id", "order_date");
-h3.setAttribute("class", "orderdate");
-h3.innerText = order_list[i]["order_date"];
-div_content.append(h3);
-
-h4 = document.createElement("h3");
-h4.setAttribute("id", "rupees");
-h4.setAttribute("class", "rupees");
-h4.innerText = "₹" + order_list[i]["price"];
-div_content.append(h4);
-
-image_flower = document.createElement("img");
-image_flower.setAttribute("id", "flower");
-image_flower.setAttribute("src", order_list[i]["img"]);
-image_flower.setAttribute("alt", order_list[i]["alt"]);
-div_content.append(image_flower);
-
-
-document.querySelector("div.main_content").append(div_content);
-// document.querySelector("div.content").append(h3);
-// document.querySelector("div.content").append(h4);
-// document.querySelector("div.content").append(image_flower);
-
-};
+    document.querySelector("div.main_content").append(div_content);
+    // document.querySelector("div.content").append(h3);
+    // document.querySelector("div.content").append(h4);
+    // document.querySelector("div.content").append(image_flower);
+  }
+});
