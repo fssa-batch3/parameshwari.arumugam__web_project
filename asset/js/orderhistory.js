6506ac80890887d6d3ec894ac4da6c825fd01d25
Parameshwari2004 <parameshwariarumugam8@gmail.com>
2023-05-07 20:55:18 +0530

    fix: fix bug in orderhistory page

--- asset/js/orderhistory.js
+++ asset/js/orderhistory.js
@@ -14,7 +14,9 @@ let activeuser = JSON.parse(localStorage.getItem("active_user"));
 //     name:"Red Rose Bouquet",
 //     order_id:"1",
 //     order_date:"23/04/2023",
-//     price:"2500"
+//     price:"2500",
+//     address:"No2,Thayagi sathya moorthi street kandanchavadi"
+      
 // }
 // ]
 
@@ -25,19 +27,11 @@ order_list.find((el) => {
     // console.log(div_content);
     // console.log(h2);
 
-    h2 = document.createElement("h2");
-    // document.querySelector("div.content").append(h2);
-    h2.setAttribute("id", "orderid");
-    h2.innerText = el["order_id"];
-
-    div_content.append(h2);
-    console.log(h2);
-
-    h3 = document.createElement("h3");
-    h3.setAttribute("id", "order_date");
-    h3.setAttribute("class", "orderdate");
-    h3.innerText = el["order_date"];
-    div_content.append(h3);
+    image_flower = document.createElement("img");
+    image_flower.setAttribute("id", "flower");
+    image_flower.setAttribute("src", el["img"]);
+    image_flower.setAttribute("alt", el["alt"]);
+    div_content.append(image_flower);
 
     h4 = document.createElement("h3");
     h4.setAttribute("id", "rupees");
@@ -45,11 +39,30 @@ order_list.find((el) => {
     h4.innerText = "₹" + el["price"];
     div_content.append(h4);
 
-    image_flower = document.createElement("img");
-    image_flower.setAttribute("id", "flower");
-    image_flower.setAttribute("src", el["img"]);
-    image_flower.setAttribute("alt", el["alt"]);
-    div_content.append(image_flower);
+    h6 = document.createElement("h6");
+    h6.setAttribute("id", "title");
+    h6.setAttribute("class", "title");
+    h6.innerText = el["tittle"];
+    div_content.append(h6);
+    
+    h3 = document.createElement("h3");
+    h3.setAttribute("id", "order_date");
+    h3.setAttribute("class", "orderdate");
+    h3.innerText = el["order_date"];
+    div_content.append(h3);
+
+    h2 = document.createElement("h2");
+    // document.querySelector("div.content").append(h2);
+    h2.setAttribute("id", "orderid");
+    h2.innerText = el["order_id"];
+    div_content.append(h2);
+    console.log(h2);
+
+    // h5 = document.createElement("h5");
+    // h5.setAttribute("id", "adress");
+    // h5.setAttribute("class", "deli_address");
+    // h5.innerText = el["address"];
+    // div_content.append(h5);
 
     document.querySelector("div.main_content").append(div_content);
     // document.querySelector("div.content").append(h3);
